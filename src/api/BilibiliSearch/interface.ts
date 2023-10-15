a50d3ca6fdca67fbc996608a6a2f132962b3a9ee
jingming295 <jingming295@gmail.com>
2023-10-15 23:33:09 +0800

    🛠️modified: 逐渐不用any

--- src/api/BilibiliSearch/interface.ts
+++ src/api/BilibiliSearch/interface.ts
@@ -9,3 +9,53 @@ interface VideoResource
     color: string;
     error: string | null;
 }
+
+interface VideoSerachData
+{
+    type: string;// 固定为video
+    id: number;// 	为稿件avid
+    author: string;// UP主昵称
+    mid: number;// UP主mid
+    typeid: string;// 视频分区tid
+    typename: string;// 视频子分区名
+    arcurl: string;// 视频重定向url
+    aid: number;// 稿件avid
+    bvid: string;// 稿件bvid
+    title: string;// 视频标题
+    description: string;// 视频简介
+    arcrank: string;// '0', 作用尚不明确
+    pic: string;// 视频封面url
+    play: number;// 视频播放量
+    video_review: number;
+    favorites: number;
+    tag: string;
+    review: number;
+    pubdate: number;
+    senddate: number;
+    duration: string;
+    badgepay: boolean;
+    hit_columns: string[];
+    view_type: string;
+    is_pay: number;
+    is_union_video: number;
+    rec_tags: null;
+    new_rec_tags: null[];
+    rank_score: number;
+    like: number;
+    upic: string;
+    corner: string;
+    cover: string;
+    desc: string;
+    url: string;
+    rec_reason: string;
+    danmaku: number;
+    biz_data: null;
+    is_charge_video: number;
+    vt: number;
+    enable_vt: number;
+    vt_display: string;
+    subtitle: string;
+    episode_count_text: string;
+    release_status: number;
+    is_intervene: number;
+}
